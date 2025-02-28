78df0d93fde2da88704dac93e021dd70abb238d9
Olmos Normominov <olmosbeknormuminov02@gmail.com>
2025-02-28 23:56:57 +0500

    k

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
 from shop.models import Product, Category
@@ -49,20 +50,21 @@ def product_create(request):
 @login_required(login_url='/admin/')
 def product_update(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    form = ProductForm(instance=product)
-
     if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
             product = form.save(commit=False)
             product.save()
-
             return redirect('index')
+    else:
+        form = ProductForm(instance=product)
 
     context = {
         'form': form
     }
-
     return render(request, 'shop/edit-product.html', context)
+
+
 
 
 @login_required(login_url='/admin/')
@@ -71,6 +73,22 @@ def product_delete(request, product_id):
     product.delete()
     return redirect('index')
 
+
+
+def product_list(request, filter_by=None):
+    products = Product.objects.all()
+
+    if filter_by ==  'Likes':
+        products = products.order_by('-Likes')
+    elif filter_by == 'Expensive':
+        products = products.order_by('-price')
+    elif filter_by == 'Cheap':
+        products = products.order_by('price')
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'shop/home.html', context)
 
 
 
